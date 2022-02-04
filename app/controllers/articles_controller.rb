51f95bf470f8c4cfe5280e2790c1300f9448cb6b
Mazen <mazenaboelanin@gmail.com>
2022-02-05 00:29:29 +0200

    add Edit Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,32 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
 
     def create 
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = 'Article was Successfully Created'
+            flash[:notice] = 'Article is Successfully Created'
             redirect_to article_path(@article)
         else 
             render 'new'
         end
     end
+
+    def update
+
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'Article is Updated Successfully'
+            redirect_to article_path(@article)
+        else 
+            render 'edit'
+        end
+    end
+
 
     def show 
         @article = Article.find(params[:id])
