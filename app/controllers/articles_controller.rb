2b0f116024e2407fb4b0f4d7a1fb857d855ba23f
Mazen <mazenaboelanin@gmail.com>
2022-02-18 22:53:23 +0200

    add - delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,6 +41,14 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article has been Deleted Successfully"
+        redirect_to articles_path
+    end
+
+
     private
     def article_params
         params.require(:article).permit(:title, :description)
