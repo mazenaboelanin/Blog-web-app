5e2f17a57745accc953cdc8876a15013853cf1c9
Mazen <mazenaboelanin@gmail.com>
2022-03-06 10:25:03 +0200

    create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show 
+        @user = User.find(params[:id])
+    end
+    
     def create 
         @user = User.new(users_params)
         if @user.save 
