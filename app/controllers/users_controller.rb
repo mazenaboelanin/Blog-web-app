a0e7cc782e4593a3c4a9197d71572eefc18b6fee
Mazen <mazenaboelanin@gmail.com>
2022-03-06 09:45:19 +0200

    add edit update function and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update 
+        @user = User.find(params[:id])
+        if @user.update(users_params)
+            flash[:success] = "Your account was updated Successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+
     private
     def users_params
         params.require(:user).permit(:username, :email, :password)
